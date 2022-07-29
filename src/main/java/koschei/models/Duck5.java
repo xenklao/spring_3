2c8abe9e274fbd4b91143d7f7340fb97c58c69de
xenklao <109021940+xenklao@users.noreply.github.com>
2022-07-29 18:52:01 +0500

    models - add code

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Duck5 {
 
+    private final Egg6 egg6;
+
+    public Duck5(Egg6 egg6) {
+        this.egg6 = egg6;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо "+egg6.toString()+"";
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 deth8;
 
     @Override
     public String toString() {
